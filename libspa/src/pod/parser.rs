4e3e605f1578254d8809bb7cee7f1fd7bc69187d
Tom A. Wagner <tom.a.wagner@protonmail.com>
2023-08-10 13:17:34 +0200

    spa pod parser: Add parser_get convenience macro to parse complex pods in single statement

--- libspa/src/pod/parser.rs
+++ libspa/src/pod/parser.rs
@@ -353,3 +353,333 @@ impl<'d> Parser<'d> {
         }
     }
 }
+
+/// Convenience macro to parse values from a spa pod using a spa pod parser.
+///
+/// For arguments, the macro accepts the parser, and then the structure of the desired pods:
+///
+/// ```ignore
+/// parser_get(<&mut libspa::pod::parser::Parser>, Bool(<&mut bool>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Id(<&mut libspa::utils::Id>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Int(<&mut i32>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Long(<&mut i64>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Float(<&mut f32>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Double(<&mut f64>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Bytes(<&mut &[u8]>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Pointer(<&mut *const c_void>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Fd(<&mut i64>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Rectangle(<&mut libspa::utils::Rectangle>));
+/// parser_get(<&mut libspa::pod::parser::Parser>, Fraction(<&mut libspa::utils::Fraction>));
+/// parser_get(<&mut libspa::pod::parser::Parser>,
+///     Struct {
+///         // 0 to n fields, e.g.:
+///         Struct {
+///             Int(<&mut i32>),
+///             Float(<&mut f32>),
+///         },
+///         Bytes(<&mut &[u8]),
+///     }
+/// );
+/// ```
+///
+/// # Returns
+///
+/// The macro returns a `Result<(), Errno>`.
+/// If parsing succeeds, an `Ok(())` is returned.
+/// Otherwise, the `Err(Errno)` from the point where parsing failed is returned, and the rest of the values are not parsed.
+#[macro_export]
+macro_rules! __parser_get__ {
+    ($parser:expr, Bool($val:expr)) => {
+        {
+            let val: &mut bool = $val;
+            let res = $crate::pod::parser::Parser::get_bool($parser);
+            if let Ok(bool) = res {
+                *val = bool;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Id($val:expr)) => {
+        {
+            let val: &mut $crate::utils::Id = $val;
+            let res = $crate::pod::parser::Parser::get_id($parser);
+            if let Ok(id) = res {
+                *val = id;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Int($val:expr)) => {
+        {
+            let val: &mut i32 = $val;
+            let res = $crate::pod::parser::Parser::get_int($parser);
+            if let Ok(int) = res {
+                *val = int;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Long($val:expr)) => {
+        {
+            let val: &mut i64 = $val;
+            let res = $crate::pod::parser::Parser::get_long($parser);
+            if let Ok(long) = res {
+                *val = long;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Float($val:expr)) => {
+        {
+            let val: &mut f32 = $val;
+            let res = $crate::pod::parser::Parser::get_float($parser);
+            if let Ok(float) = res {
+                *val = float;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Double($val:expr)) => {
+        {
+            let val: &mut f64 = $val;
+            let res = $crate::pod::parser::Parser::get_double($parser);
+            if let Ok(double) = res {
+                *val = double;
+            }
+            res.map(|_| {})
+        }
+    };
+    // TODO: String
+    ($parser:expr, Bytes($val:expr)) => {
+        {
+            let val: &mut &[u8] = $val;
+            let res = $crate::pod::parser::Parser::get_bytes($parser);
+            if let Ok(bytes) = res {
+                *val = bytes;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Pointer($val:expr)) => {
+        {
+            let val: &mut (*const c_void, Id) = $val;
+            let res = $crate::pod::parser::Parser::get_pointer($parser);
+            if let Ok(ptr) = res {
+                *val = ptr;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Fd($val:expr)) => {
+        {
+            let val: &mut i64 = $val;
+            let res = $crate::pod::parser::Parser::get_fd($parser);
+            if let Ok(fd) = res {
+                *val = fd;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Rectangle($val:expr)) => {
+        {
+            let val: &mut $crate::utils::Rectangle = $val;
+            let res = $crate::pod::parser::Parser::get_rectangle($parser);
+            if let Ok(rect) = res {
+                *val = rect;
+            }
+            res.map(|_| {})
+        }
+    };
+    ($parser:expr, Fraction($val:expr)) => {
+        {
+            let val: &mut $crate::utils::Fraction = $val;
+            let res = $crate::pod::parser::Parser::get_fraction($parser);
+            if let Ok(int) = res {
+                *val = int;
+            }
+            res.map(|_| {})
+        }
+    };
+    // TODO: ($parser:expr, Pod($val:expr))
+    ($parser:expr, Struct { $( $field_type:tt $field:tt ),* $(,)? }) => {
+        'outer: {
+            let mut frame: ::std::mem::MaybeUninit<$crate::sys::spa_pod_frame> = ::std::mem::MaybeUninit::uninit();
+            let res = unsafe { $crate::pod::parser::Parser::push_struct($parser, &mut frame) };
+            if res.is_err() {
+                break 'outer res;
+            }
+
+            $(
+                let res = __parser_get__!($parser, $field_type $field);
+                if res.is_err() {
+                    // Discard Ok variant value so we can assign to Result<(), Errno>
+                    break 'outer res.map(|_| {});
+                }
+            )*
+
+            unsafe { $crate::pod::parser::Parser::pop($parser, frame.assume_init_mut()) }
+        }
+    };
+    // TODO: Object
+    // TODO: ($parser:expr, Option( $type_:tt $val:tt )) or similar for optional values
+}
+pub use __parser_get__ as parser_get;
+
+#[cfg(test)]
+mod tests {
+    use super::{parser_get, Parser};
+
+    // FIXME: The way we construct raw pods here is rather crude and error-prone.
+    //        Maybe replace it with the pod builder in the future, and share the tests with it.
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn parse_bool() {
+        let pod: Vec<u8> = [
+            &4u32.to_ne_bytes(), // bool body size
+            &2u32.to_ne_bytes(), // bool type
+            &1u32.to_ne_bytes(), // bool "true"
+            &[0, 0, 0, 0],       // padding
+        ]
+        .into_iter()
+        .flatten()
+        .copied()
+        .collect();
+
+        let mut parser = Parser::new(&pod);
+        let mut bool = false;
+
+        let res = parser_get!(&mut parser, Bool(&mut bool));
+
+        assert!(res.is_ok());
+        assert!(bool);
+    }
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn parse_empty_struct() {
+        let pod: Vec<u8> = [
+            &0u32.to_ne_bytes(),  // body size: 0 children => 0 bytes
+            &14u32.to_ne_bytes(), // struct type
+        ]
+        .into_iter()
+        .flatten()
+        .copied()
+        .collect();
+
+        let mut parser = Parser::new(&pod);
+
+        let res = parser_get!(&mut parser, Struct {});
+
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn parse_complicated_struct() {
+        let pod: &[&[u8]] = &[
+            &168u32.to_ne_bytes(), // body size: (1 child * 104 bytes) + (4 children * 16 bytes per child) = 168 bytes
+            &14u32.to_ne_bytes(),  // struct type
+            // begin inner struct
+            &96u32.to_ne_bytes(), // body size: (6 children * 16 bytes per child) = 96 bytes
+            &14u32.to_ne_bytes(), // struct type
+            &4u32.to_ne_bytes(),  // bool body size
+            &2u32.to_ne_bytes(),  // bool type
+            &1u32.to_ne_bytes(),  // bool "true"
+            &[0, 0, 0, 0],        // padding
+            &4u32.to_ne_bytes(),  // id body size
+            &3u32.to_ne_bytes(),  // id type
+            &313u32.to_ne_bytes(), // id 313
+            &[0, 0, 0, 0],        // padding
+            &4u32.to_ne_bytes(),  // int body size
+            &4u32.to_ne_bytes(),  // int type
+            &313i32.to_ne_bytes(), // int 313
+            &[0, 0, 0, 0],        // padding
+            &8u32.to_ne_bytes(),  // long body size
+            &5u32.to_ne_bytes(),  // long type
+            &313i64.to_ne_bytes(), // long 313
+            &4u32.to_ne_bytes(),  // float body size
+            &6u32.to_ne_bytes(),  // float type
+            &31.3f32.to_ne_bytes(), // float 31.3
+            &[0, 0, 0, 0],        // padding
+            &8u32.to_ne_bytes(),  // double body size
+            &7u32.to_ne_bytes(),  // double type
+            &31.3f64.to_ne_bytes(), // double 31.3
+            // end inner struct
+            &3u32.to_ne_bytes(),   // bytes body size
+            &9u32.to_ne_bytes(),   // bytes type
+            &[3, 1, 3],            // bytes [3u8, 1u8, 3u8]
+            &[0, 0, 0, 0, 0],      // padding
+            &8u32.to_ne_bytes(),   // fd body size
+            &18u32.to_ne_bytes(),  // fd type
+            &313i64.to_ne_bytes(), // fd 313
+            &8u32.to_ne_bytes(),   // rectangle body size
+            &10u32.to_ne_bytes(),  // rectangle type
+            &313u32.to_ne_bytes(), // rectangle width 313
+            &131u32.to_ne_bytes(), // rectangle height 131
+            &8u32.to_ne_bytes(),   // fraction body size
+            &11u32.to_ne_bytes(),  // fraction type
+            &313u32.to_ne_bytes(), // fraction num 313
+            &131u32.to_ne_bytes(), // fraction denom 131
+        ];
+        let pod: Vec<u8> = pod.iter().flat_map(|f| (*f)).copied().collect();
+
+        let mut parser = Parser::new(&pod);
+
+        let mut bool = false;
+        let mut id = crate::utils::Id(0);
+        let mut int = 0i32;
+        let mut long = 0i64;
+        let mut float = 0.0f32;
+        let mut double = 0.0f64;
+        let mut bytes: &[u8] = &[];
+        let mut fd = 0i64;
+        let mut rect = crate::utils::Rectangle {
+            width: 0,
+            height: 0,
+        };
+        let mut frac = crate::utils::Fraction { num: 0, denom: 1 };
+
+        let res = parser_get!(
+            &mut parser,
+            Struct {
+                Struct {
+                    Bool(&mut bool),
+                    Id(&mut id),
+                    Int(&mut int),
+                    Long(&mut long),
+                    Float(&mut float),
+                    Double(&mut double),
+                },
+                Bytes(&mut bytes),
+                Fd(&mut fd),
+                Rectangle(&mut rect),
+                Fraction(&mut frac),
+            }
+        );
+
+        assert!(res.is_ok());
+        assert!(bool);
+        assert_eq!(id, crate::utils::Id(313));
+        assert_eq!(int, 313);
+        assert_eq!(long, 313);
+        assert_eq!(float, 31.3);
+        assert_eq!(double, 31.3);
+        assert_eq!(bytes, &[3, 1, 3]);
+        assert_eq!(fd, 313);
+        assert_eq!(
+            rect,
+            crate::utils::Rectangle {
+                width: 313,
+                height: 131
+            }
+        );
+        assert_eq!(
+            frac,
+            crate::utils::Fraction {
+                num: 313,
+                denom: 131
+            }
+        );
+    }
+}
