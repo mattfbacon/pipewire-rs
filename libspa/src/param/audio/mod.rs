2bdcf8dfb79bb4c9555a4080544a6d7b2f22304d
Dorinda Bassey <dbassey@redhat.com>
2024-01-09 23:48:24 +0100

    Add missing audioformats
    Add missing audioformats

--- libspa/src/param/audio/mod.rs
+++ libspa/src/param/audio/mod.rs
@@ -48,6 +48,8 @@ impl AudioFormat {
     pub const F32BE: Self = Self(spa_sys::SPA_AUDIO_FORMAT_F32_BE);
     pub const F64LE: Self = Self(spa_sys::SPA_AUDIO_FORMAT_F64_LE);
     pub const F64BE: Self = Self(spa_sys::SPA_AUDIO_FORMAT_F64_BE);
+    pub const ULAW: Self = Self(spa_sys::SPA_AUDIO_FORMAT_ULAW);
+    pub const ALAW: Self = Self(spa_sys::SPA_AUDIO_FORMAT_ALAW);
 
     pub const U8P: Self = Self(spa_sys::SPA_AUDIO_FORMAT_U8P);
     pub const S16P: Self = Self(spa_sys::SPA_AUDIO_FORMAT_S16P);
